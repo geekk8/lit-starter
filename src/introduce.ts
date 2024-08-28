489386f5fe058842e4c2d6203fbba93c409636d1
Oslo <Oslo.noh@bunjang.co.kr>
2024-08-28 22:38:55 +0900

    lit customElement test

---
+++ src/introduce.ts
@@ -0,0 +1,22 @@
+import { html, LitElement } from "lit";
+import { customElement, property } from "lit/decorators";
+
+@customElement('introduce')
+export class introduce extends LitElement {
+  @property({type:String})
+  name = 'noh kuk hyun'
+  job = 'developer'
+
+  @property({type:Number})
+  age = 34
+
+  getMyIntroudceData = () => {
+
+  }
+
+  override render() {
+    html`
+
+    `
+  }
+}
